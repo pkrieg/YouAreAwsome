8b6371999ec98d5a6e825a0540b6a687c03d0eec
Peter Krieger <krieger.peter1@gmx.de>
2025-08-25 11:08:30 +0200

    Ch. 1.3 Button Styles, Spacers, Frames, & Optional Parameters (2026)

--- YouAreAwsome/ContentView.swift
+++ YouAreAwsome/ContentView.swift
@@ -12,24 +12,36 @@ struct ContentView: View {
     @State private var message = "I am a Programmer!"
     
     var body: some View {
+        Spacer()
         
         VStack(alignment: .center) {
+            
             Image(systemName: "swift")
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
+                .frame(width: 200, height: 200)
+            
             Text(message)
                 .font(.largeTitle)
-                .fontWeight(.bold)
-                .foregroundStyle(.blue)
+                .fontWeight(.ultraLight)
         }
-        // new stuff here!
-        Button("Click me!") {
-            message = "Awsome!"
-            print(message)
+        
+        Spacer()
+        Spacer()
+        
+        HStack {
+            Button("Awsome") {
+                message = "Awsome!"
+                print(message)
+            }
+            Button("Great!") {
+                message = "Great!"
+            }
         }
-
-
+        .buttonStyle(.borderedProminent)
+        .font(.title2)
+        .tint(.orange)
     }
 }
 
